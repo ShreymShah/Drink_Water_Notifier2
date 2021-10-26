dc96855a60dc4b827c0026e1f59f1378a55f32d6
ShreymShah <93068854+ShreymShah@users.noreply.github.com>
2021-10-26 16:33:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import requests
 tz = pytz.timezone('Asia/Kolkata')
 now = datetime.now(tz)
 true_or_not = True
+true_or_not2 = True
 
 while 8 < now.hour < 22:
     now = datetime.now(tz)
     if str(now.minute) == '01':
         true_or_not = True
+        
+    elif str(now.minute) == '31':
+        true_or_not2 = True
 
     telegram_token = '2058300469:AAFNOABY1Ay5ocvzASNGhgUyuKsQGhEGVHU'
     telegram_chat_id = '773247730'
@@ -23,6 +27,10 @@ while 8 < now.hour < 22:
         send_msg_telegram("DRINK WATER")
         send_msg_telegram("DRINK WATER")
         true_or_not = False
+        
+    elif now.minute == 30 and true_or_not2 == True:
+        send_msg_telegram("30 minutes - Drink Water")
+        true_or_not2 = False
 
 
 
